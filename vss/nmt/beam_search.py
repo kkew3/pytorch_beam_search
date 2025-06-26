a96c49ee1584dcf24c6ebd151f3af4738cd2152e
kkew3 <kps6326@hotmail.com>
2025-06-26 12:13:19 +0800

    Make the beam search code more naive, but should be correct

--- vss/nmt/beam_search.py
+++ vss/nmt/beam_search.py
@@ -1,4 +1,4 @@
-from typing import Callable
+from typing import Callable, Literal
 
 import torch
 from torch import Tensor
@@ -143,21 +143,39 @@ def test_get_tensor_by_flattened_index():
     ).all()
 
 
+class NormalizedLogProb(float):
+    pass
+
+
+def normalize_log_prob(
+    log_prob: float,
+    length: int,
+    length_normalization: float,
+) -> NormalizedLogProb:
+    return NormalizedLogProb(log_prob / length**length_normalization)
+
+
 def naive_beam_search(
-    model: Callable[[Tensor], Tensor],
+    model: Callable[[Tensor, Tensor, Tensor], Tensor],
     bos_token_id: int,
     eos_token_id: int,
     beam_width: int,
     max_length: int,
     length_normalization: float,
-) -> Tensor:
+    input_ids: Tensor,
+    attention_mask: Tensor,
+    device: Literal['cpu', 'cuda'],
+) -> tuple[list[Tensor], Tensor]:
     """
     Parameters
     ----------
-    model : (LongTensor) -> FloatTensor
-        The language model that takes as input the input ids, of shape
-        (batch_size, seq_len) and outputs the logits of a batch of next tokens,
-        of shape (batch_size, vocab_size).
+    model : (LongTensor, LongTensor, LongTensor, LongTensor) -> FloatTensor
+        The language model, should be callable as `model(input_ids,
+        attention_mask, decoder_input_ids)` and return a batch
+        of next token logits. Shapes: input_ids---(batch_size,
+        enc_seq_len), attention_mask---(batch_size, enc_seq_len),
+        decoder_input_ids---(batch_size, dec_seq_len), return
+        value---(batch_size, vocab_size).
     bos_token_id : int
         The beginning-of-sequence token id.
     eos_token_id : int
@@ -169,52 +187,98 @@ def naive_beam_search(
     length_normalization : float
         The length normalization factor. Set to 0 to disable length
         normalization.
+    input_ids : LongTensor
+        The encoder input ids of shape (batch_size, enc_seq_len).
+    attention_mask : LongTensor
+        The encoder attention mask of shape (batch_size, enc_seq_len).
+    device : 'cpu' | 'cuda'
+        The device.
 
     Returns
     -------
-    samples : LongTensor
-        The most likely sequence, of shape (seq_len,) where seq_len is no
-        larger than max_length.
+    samples : list[LongTensor]
+        The most likely sequences of length batch_size. The i-th element is of
+        shape (seq_len_i,) where seq_len_i <= max_length.
+    normalized_log_prob : FloatTensor
+        The normalized log prob of `sample`, of shape (batch_size,).
     """
     assert beam_width >= 1
     assert max_length >= 1
 
-    topk_candidates = torch.tensor([bos_token_id]).unsqueeze_(0)
-    topk_log_probs = torch.zeros(1)
-    # Element i shape: (k_i, seq_len_i).
-    completed_candidates = []
-    # Element i shape: (k_i,).
-    completed_candidate_scores = []
-    for step in range(1, 1 + max_length):
-        # topk_candidates shape: (k, step), where k <= beam_width.
-        # log_probs shape: (vocab_size, k).
-        log_probs = model(topk_candidates).log_softmax(-1).t()
-        # topk_log_probs shape: (k,).
-        # cum_log_probs shape: (vocab_size, k).
-        cum_log_probs = topk_log_probs + log_probs
-        # token_ids & seq_ids shape: (beam_width,).
-        _, (token_ids, seq_ids) = topk_2d(
-            cum_log_probs / step**length_normalization, beam_width
+    # Process the batch of inputs one at a time.
+    batch_size = input_ids.size(0)
+    final_samples = []
+    final_normalized_log_probs = []
+    for i in range(batch_size):
+        # Form singleton batch of inputs.
+        # shape: (1, enc_seq_len)
+        curr_input_ids = input_ids[i : i + 1]
+        # shape: (1, enc_seq_len)
+        curr_attention_mask = attention_mask[i : i + 1]
+        # curr_beam: a list of candidate (dec_input_ids, unnormalized
+        # log_prob).
+        curr_beam: list[tuple[list[int], float]] = [([bos_token_id], 0.0)]
+        completed_sequences: list[tuple[list[int], NormalizedLogProb]] = []
+        for step in range(1, 1 + max_length):
+            all_candidates: list[tuple[list[int], float]] = []
+            curr_beam_size = len(curr_beam)
+            for _ in range(curr_beam_size):
+                dec_input_ids, unnormalized_log_prob = curr_beam.pop()
+                # The log prob of the next token.
+                # shape: (vocab_size,)
+                next_log_probs = (
+                    model(
+                        curr_input_ids,
+                        curr_attention_mask,
+                        torch.tensor([dec_input_ids], device=device),
+                    )
+                    .squeeze_(0)
+                    .log_softmax(0)
+                )
+                vocab_size = next_log_probs.size(0)
+                for j in range(vocab_size):
+                    all_candidates.append(
+                        (
+                            dec_input_ids + [j],
+                            unnormalized_log_prob
+                            + float(next_log_probs[j].item()),
+                        )
+                    )
+            all_candidates.sort(
+                key=lambda x: normalize_log_prob(
+                    x[1], step, length_normalization
+                ),
+                reverse=True,
+            )
+            topk_candidates = all_candidates[:beam_width]
+            for dec_input_ids, unnormalized_log_prob in topk_candidates:
+                last_token = dec_input_ids[-1]
+                if last_token == eos_token_id:
+                    normalized_log_prob = normalize_log_prob(
+                        unnormalized_log_prob, step, length_normalization
+                    )
+                    completed_sequences.append(
+                        (dec_input_ids, normalized_log_prob)
+                    )
+                    beam_width -= 1
+                else:
+                    curr_beam.append((dec_input_ids, unnormalized_log_prob))
+        for dec_input_ids, unnormalized_log_prob in curr_beam:
+            completed_sequences.append(
+                (
+                    dec_input_ids,
+                    normalize_log_prob(
+                        unnormalized_log_prob, max_length, length_normalization
+                    ),
+                )
+            )
+        dec_input_ids, normalized_log_prob = max(
+            completed_sequences, key=lambda x: x[1]
         )
-        # topk_log_probs shape: (beam_width,).
-        topk_log_probs = cum_log_probs[token_ids, seq_ids]
-        # topk_candidates shape: (beam_width, step + 1).
-        topk_candidates = grow_candidates(topk_candidates, token_ids, seq_ids)
-        # is_completed shape: (beam_width,).
-        is_completed = token_ids == eos_token_id
-        completed_candidates.append(topk_candidates[is_completed, 1:])
-        completed_candidate_scores.append(topk_log_probs[is_completed])
-        # topk_candidates shape: (k, step + 1), where k <= beam_width.
-        topk_candidates = topk_candidates[~is_completed]
-        # topk_log_probs shape: (k,).
-        topk_log_probs = topk_log_probs[~is_completed]
-        # is_completed.sum() <= beam_width.
-        beam_width -= is_completed.sum()
-        if beam_width == 0:
-            break
-    if topk_candidates.size(0) > 0:
-        completed_candidates.append(topk_candidates[:, 1:])
-        completed_candidate_scores.append(topk_log_probs)
-    # Get the index of the top-1 sequence.
-    j = torch.cat(completed_candidate_scores, 0).argmax(0)
-    return get_tensor_by_flattened_index(completed_candidates, j)
+        # Strip off the bos token at position 0.
+        final_samples.append(torch.tensor(dec_input_ids[1:], device=device))
+        final_normalized_log_probs.append(normalized_log_prob)
+    return (
+        final_samples,
+        torch.tensor(final_normalized_log_probs, device=device),
+    )
